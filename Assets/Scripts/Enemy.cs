120218dd41b93cacb5fb845e4a0fca11df180a73
Tess <t.eleanor.o@gmail.com>
2018-02-23 12:02:00 -0600

    scene

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    public int enemyHordeAmount;
+
+    [SerializeField]
+    List<Rigidbody> rgd;
+
+    [SerializeField]
+    List<BoxCollider> bc;
+
+    public int hordeIncreaseTimeDelay;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        enemyHordeAmount = 5;
+
+        rgd = new List<Rigidbody>();
+
+        bc = new List<BoxCollider>();
+
+        hordeIncreaseTimeDelay = 20;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        StartCoroutine(MyCoroutine(hordeIncreaseTimeDelay));
 	}
+
+    IEnumerator MyCoroutine(int delay)
+    {
+        yield return new WaitForSeconds(3f);
+    }
 }
