74537908dd58045e7f805fcffe0ffefe86d5ab04
luke-goldfain <lukegoldfain@gmail.com>
2018-02-23 13:15:45 -0600

    added/edited shot, shotspawn, and their scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,17 +11,42 @@ public class PlayerController : MonoBehaviour {
     {
 
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    // Update is called once per frame
+    void FixedUpdate ()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
 
+        var rotateZ = 0;
+
         Rigidbody rb = GetComponent<Rigidbody>();
 
         Vector3 move = new Vector3(x, 0, z);
+        Quaternion rotate = new Quaternion(0, 0, rotateZ, 0);
 
         rb.velocity = move * speed;
+
+        if (Input.GetKeyDown("up"))
+        {
+            rotateZ = 0;
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            rotateZ = 90;
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            rotateZ = 180;
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            rotateZ = 270;
+        }
+
+        rb.transform.rotation = rotate;
 	}
 }
---
+++ Assets/Scripts/Schut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schut : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+    private int shotDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+
+
+        }
+	}
+}
