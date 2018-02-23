868f45111af927143174e1c126d16222793d0b81
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-02-23 15:20:20 -0600

    workin on the health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    float startingHealth = 100;
+    float currentHealth;
+
+    [SerializeField]
+    private Slider slider;
+
+    private Color fullColor = Color.green;
+    private Color emptyColor = Color.red;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CauseDamage(float amountOfDamage)
+    {
+        currentHealth -= amountOfDamage;
+        SetSlider();
+    }
+
+    private void SetSlider()
+    {
+        slider.value = currentHealth;
+
+    }
+}
