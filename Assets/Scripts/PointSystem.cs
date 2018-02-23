41166682c0ad064fe3161220b80ed1b33dd6226b
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-02-23 14:37:30 -0600

    Deleted extra script

--- Assets/Scripts/PointSystem.cs
+++ Assets/Scripts/PointSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,23 +7,13 @@ using UnityEngine.UI;
 public class PointSystem : MonoBehaviour {
 
     [SerializeField]
-    private int pointAmount = 1;
-
     private Text text;
 
     private int points;
-
-    //public int Points//i think the only script that needs to change the point amount is this one...
-    //{
-    //    get
-    //    {
-    //        return points;
-    //    }
-    //}
     private void Awake()
     {
-        DontDestroyOnLoad(this);
         text = GetComponent<Text>();
+        DontDestroyOnLoad(this);
     }
 
 	// Use this for initialization
@@ -35,8 +26,8 @@ public class PointSystem : MonoBehaviour {
         text.text = "Points: " + points;
     }
 
-    public void AddPoints()
+    public void AddPoints(int pointamount)
     {
-        points += pointAmount;
+        points += pointamount;
     }
 }
