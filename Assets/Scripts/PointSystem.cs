875be52f0f439b701a798ca7c717124641402264
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-02-23 12:37:31 -0600

    added canvas for text display

--- Assets/Scripts/PointSystem.cs
+++ Assets/Scripts/PointSystem.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointSystem : MonoBehaviour {
 
+    [SerializeField]
+    private int pointAmount = 1;
+
+    [SerializeField]
+    private Text text;
+
     private int points;
-    private int pointAmount;
-    public int Points//i think the only script that needs to change the point amount is this one...
-    {
-        get
-        {
-            return points;
-        }
-    }
+
+    //public int Points//i think the only script that needs to change the point amount is this one...
+    //{
+    //    get
+    //    {
+    //        return points;
+    //    }
+    //}
     private void Awake()
     {
         DontDestroyOnLoad(this);
+        text = GetComponent<Text>();
     }
 
 	// Use this for initialization
@@ -23,8 +31,12 @@ public class PointSystem : MonoBehaviour {
     {
         points = 0;
 	}
+    private void Update()
+    {
+        text.text = "Points: " + points;
+    }
 
-    void AddPoints()
+    public void AddPoints()
     {
         points += pointAmount;
     }
