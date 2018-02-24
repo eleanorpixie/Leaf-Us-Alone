c8b301de39e26b46713e11ca81a7d2ecc95af369
Tess <t.eleanor.o@gmail.com>
2018-02-24 14:48:04 -0600

    scenes

--- Assets/Animation Controller/EnemyAnimationFlip.cs
+++ Assets/Animation Controller/EnemyAnimationFlip.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAnimationFlip : MonoBehaviour {
-    
+public class EnemyAnimation : MonoBehaviour
+{    
+    private SpriteRenderer spr;
+    private Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    private Rigidbody rig;
+
+	private void Start () {
         
-        Vector3 Position = transform.position;
-        
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        SpriteRenderer spr = gameObject.GetComponent<SpriteRenderer>();
-        if (gameObject.transform.position.x < 0)
+	private void Update ()
+    {
+        if (this.transform.position.x < 0)
         {
             spr.flipX = false;
         }
-        else if (gameObject.transform.position.x > 0)
+        else if (this.transform.position.x > 0)
         {
             spr.flipX = true;
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Tree")
+        {
+            anim.SetBool("IsAttacking", true);
         }
     }
 }
